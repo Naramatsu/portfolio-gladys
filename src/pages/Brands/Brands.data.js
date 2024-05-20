4ce2a07591a2c891fdbf0bfd7bac65a72b79a445
Narmatsu <joncyy_19@hotmail.com>
2024-05-20 13:43:51 -0500

    Brands component was added

---
+++ src/pages/Brands/Brands.data.js
@@ -0,0 +1,18 @@
+export const brandsCollabs = "Marcas con las que he trabajado";
+
+export const brandListLeft = [
+  [
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQGMFhMv8sTsI9xXZoehk68UZER3mkJrgLDnoxUMNU&s",
+    "https://ekimak.com/wp-content/uploads/2019/08/logo-full-color.png",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw9D09n7YA9oSmNeEG0EcIc25fmrQkaL7uMLK1d6EIOA&s",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLb8eyyofUlCFUZIFBZznqnUYeWCPVPd8FXa_gM0xbqA&s",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqgcr5RmChBsw8g5rLQDrNp7XB1b2OJfMMUHgpel7i&s",
+  ],
+  [
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQGMFhMv8sTsI9xXZoehk68UZER3mkJrgLDnoxUMNU&s",
+    "https://ekimak.com/wp-content/uploads/2019/08/logo-full-color.png",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw9D09n7YA9oSmNeEG0EcIc25fmrQkaL7uMLK1d6EIOA&s",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLb8eyyofUlCFUZIFBZznqnUYeWCPVPd8FXa_gM0xbqA&s",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqgcr5RmChBsw8g5rLQDrNp7XB1b2OJfMMUHgpel7i&s",
+  ],
+];
